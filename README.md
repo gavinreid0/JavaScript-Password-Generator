c2d99f43c24f93a1d43f8ca9015294252cf030b3
Gavin Reid <gavinreid94@gmail.com>
2021-05-22 19:51:11 -0500

    added HTML, CSS, Java, and readme files to repository.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Password-Generator
+
+## Desciption
+    This application Utilizes the use of HTML, CSS, and Javascript to generate a random, secure 
+    assword for the user. This enables employees to generate random passwords based on criteria 
+    that they’ve selected. This app runs in the browser and features dynamically updated HTML 
+    and CSS powered by JavaScript code. This assignment illuminates the use of Javascript to 
+    make dynamic changes to an HMTL document. 
+
+## Link to Application 
+The password generator is currently hosted by GitPages from a GitHub repository.
+
+[Click here to see the deployed application!](https://gavinreid0.github.io/Password-Generator/)
+
+## Usage
+ * a user can generate a password by clicking the red "Generate Password" button. 
+ * After clicking the user will be prompted to choose a value between 8 & 128 to determine how many characters their password should be.
+ * Next the user will be alerted to select critia for there password including ("Will this contain numbers?", "Will this contain special characters?", "Will this contain Uppercase letters?", & "Will this contain Lowercase letters?") and the user may choose yes or no by clicking "cancel" for NO and "ok" for YES.
+ * The text box will display the users password once it is generated. 
+
+The following image shows the web application's apperance and functionality:
+![](./Assets/PasswordGeneratorSS.png)
+![](./Assets/PasswordGeneratorSS2.png)
+
---
+++ script.js
@@ -0,0 +1,101 @@
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
+
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+alpha2 = ["A", "B", "C", "D", "E", "F", "G", "G", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "Q", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var choices;
+// Assignment Code
+var get = document.querySelector("#generate");
+// Add event listener to generate button
+get.addEventListener("click", function () {
+    pswrd = generatePassword();
+    document.getElementById("password").placeholder = pswrd;
+});
+
+function generatePassword() {
+    
+    enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+    
+    if (!enter) {
+        alert("This needs a value");
+    } else if (enter < 8 || enter > 128) {
+        enter = parseInt(prompt("Please specify how long you would like your password to be (between 8 and 128)"));
+    } else {
+        
+        confirmNumber = confirm("Will this contain numbers?");
+        confirmCharacter = confirm("Will this contain special characters?");
+        confirmUppercase = confirm("Will this contain Uppercase letters?");
+        confirmLowercase = confirm("Will this contain Lowercase letters?");
+    };
+    
+    if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+        choices = alert("You must choose a criteria!");
+    }
+    
+    else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
+        choices = character.concat(number, alpha, alpha2);
+    }
+    
+    else if (confirmCharacter && confirmNumber && confirmUppercase) {
+        choices = character.concat(number, alpha2);
+    }
+    else if (confirmCharacter && confirmNumber && confirmLowercase) {
+        choices = character.concat(number, alpha);
+    }
+    else if (confirmCharacter && confirmLowercase && confirmUppercase) {
+        choices = character.concat(alpha, alpha2);
+    }
+    else if (confirmNumber && confirmLowercase && confirmUppercase) {
+        choices = number.concat(alpha, alpha2);
+    }
+    
+    else if (confirmCharacter && confirmNumber) {
+        choices = character.concat(number);
+
+    } else if (confirmCharacter && confirmLowercase) {
+        choices = character.concat(alpha);
+
+    } else if (confirmCharacter && confirmUppercase) {
+        choices = character.concat(alpha2);
+    }
+    else if (confirmLowercase && confirmNumber) {
+        choices = alpha.concat(number);
+
+    } else if (confirmLowercase && confirmUppercase) {
+        choices = alpha.concat(alpha2);
+
+    } else if (confirmNumber && confirmUppercase) {
+        choices = number.concat(alpha2);
+    }
+    // Else if for 1 positive option
+    else if (confirmCharacter) {
+        choices = character;
+    }
+    else if (confirmNumber) {
+        choices = number;
+    }
+    else if (confirmLowercase) {
+        choices = alpha;
+    }
+    
+    var password = [];
+   
+    for (var i = 0; i < enter; i++) {
+        var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+        password.push(pickChoices);
+    }
+    
+    var pswrd = password.join("");
+    UserInput(pswrd);
+    return pswrd;
+}
+
+function UserInput(pswrd) {
+    document.getElementById("password").textContent = pswrd;
+}
