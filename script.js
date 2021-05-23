dcf57264348f054ed3b41f99794b35a391f56e33
Gavin Reid <gavinreid94@gmail.com>
2021-05-22 19:58:45 -0500

    commented Java

--- script.js
+++ script.js
@@ -19,29 +19,28 @@ get.addEventListener("click", function () {
 });
 
 function generatePassword() {
-    
     enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
-    
+    // if statement for user validation 
     if (!enter) {
         alert("This needs a value");
     } else if (enter < 8 || enter > 128) {
         enter = parseInt(prompt("Please specify how long you would like your password to be (between 8 and 128)"));
     } else {
-        
+        // Continues once user input is validated
         confirmNumber = confirm("Will this contain numbers?");
         confirmCharacter = confirm("Will this contain special characters?");
         confirmUppercase = confirm("Will this contain Uppercase letters?");
         confirmLowercase = confirm("Will this contain Lowercase letters?");
     };
-    
+    // Else if for 4 negative options
     if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
         choices = alert("You must choose a criteria!");
     }
-    
+    // Else if for 4 positive options
     else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
         choices = character.concat(number, alpha, alpha2);
     }
-    
+    // Else if for 3 positive options
     else if (confirmCharacter && confirmNumber && confirmUppercase) {
         choices = character.concat(number, alpha2);
     }
@@ -54,7 +53,7 @@ function generatePassword() {
     else if (confirmNumber && confirmLowercase && confirmUppercase) {
         choices = number.concat(alpha, alpha2);
     }
-    
+    // Else if for 2 positive options 
     else if (confirmCharacter && confirmNumber) {
         choices = character.concat(number);
 
@@ -83,19 +82,19 @@ function generatePassword() {
     else if (confirmLowercase) {
         choices = alpha;
     }
-    
+    // Placeholder for user generated amount of length
     var password = [];
-   
+    // Random selection for all variables: 
     for (var i = 0; i < enter; i++) {
         var pickChoices = choices[Math.floor(Math.random() * choices.length)];
         password.push(pickChoices);
     }
-    
+    // Joins password array and converts it to a string
     var pswrd = password.join("");
     UserInput(pswrd);
     return pswrd;
 }
-
+// Puts password value into the textbox.
 function UserInput(pswrd) {
     document.getElementById("password").textContent = pswrd;
 }
